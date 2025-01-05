15f65a6b19ab6fa7b5f6d52252e2c482a2893b3a
Santhosh20112003 <shanmugamsanthosh22@gmail.com>
2025-01-06 04:52:50 +0530

    first commit

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 const express = require("express");
 const { Resend } = require("resend");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 
-const resend = new Resend("re_VyJ9Znci_7YSzHNCroyLA9jx4Jq1dSZxY");
+const resendApiKey = process.env.RESEND_API_KEY;
+const adminkey = process.env.ADMIN_KEY;
+if (!resendApiKey) {
+  console.error("RESEND_API_KEY environment variable not set!");
+  process.exit(1);
+}
+
+const resend = new Resend(resendApiKey);
 app.post("/send-email", async (req, res) => {
   try {
-    const { to, subject, html } = req.body;
-    if (!to || !subject || !html) {
-      return res
-        .status(400)
-        .json({ error: "Missing required fields (to, subject, html)" });
+    const { to, subject, body } = req.body;
+    const token = req.headers.token;
+
+    // console.log(token);
+
+    if (!token || token !== adminkey) {
+      return res.status(401).json({ error: "Unauthorized" }); // 401 Unauthorized
+    }
+
+    if (!to) {
+      return res.status(400).json({ error: "Missing required fields (to)" });
     }
 
     const emailOptions = {
       from: "support@santechh.online",
-      to: to,
-      subject: subject,
-      html: html,
+      to,
+      subject: body || "Welcome to Santech Community",
+      html: subject || `Get ready to explore the more products from us.`,
     };
 
     const response = await resend.emails.send(emailOptions);
